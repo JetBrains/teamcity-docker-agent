ad3d95d282cb76475839fb4ceac73576ae4c4cfa
Sami Jawhar <sami.n.jawhar@gmail.com>
2017-09-22 14:37:17 -0700

    Added the ability to log in to docker hub
    For people who use private repos and images

--- run-docker.sh
+++ run-docker.sh
@@ -3,4 +3,7 @@
 if [ "$DOCKER_IN_DOCKER" = "start" ] ; then
  service docker start
  echo "Docker daemon started"
+ if [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
+  docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
+ fi
 fi
