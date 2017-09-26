3c027629c7558b10fa3c83ed2fec90d9858cbd76
Sami Jawhar <sami.n.jawhar@gmail.com>
2017-09-25 19:31:35 -0700

    Added $DOCKER_REGISTRY to run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -4,6 +4,6 @@ if [ "$DOCKER_IN_DOCKER" = "start" ] ; then
  service docker start
  echo "Docker daemon started"
  if [ ! -z "$DOCKER_USERNAME" ] && [ ! -z "$DOCKER_PASSWORD" ]; then
-  docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD
+  docker login --username=$DOCKER_USERNAME --password=$DOCKER_PASSWORD $DOCKER_REGISTRY
  fi
 fi
