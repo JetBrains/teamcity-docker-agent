5d37835139faa5e15545598899a09f5ca476dbe2
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2019-08-14 17:51:18 +0200

    TW-61433: TeamCity ARM template: windows docker container has no runners
     - Use teamcity-agent:windowsservercore-1809 as a source for tools in nanoserver-1903
     - Build 1903 version of nanoserver

--- build.ps1
+++ build.ps1
@@ -1,10 +1,12 @@
-$tags = @("1803", "1809")
+$nanoserverTags = @("1803", "1809", "1903")
+$servercoreTags = @("1803", "1809")
 
-foreach ($tag in $tags) {
+foreach ($tag in $nanoserverTags) {
  docker build `
   -t "teamcity-agent:latest-windowsservercore-$tag" `
   -f "windowsservercore/$tag/Dockerfile" .
-
+}
+foreach ($tag in $nanoserverTags) {
  docker build `
   -t "teamcity-agent:latest-nanoserver-$tag" `
   -f "nanoserver/$tag/Dockerfile" .
