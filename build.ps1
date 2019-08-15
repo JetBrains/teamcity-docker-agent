d7831aa5cd02eea365f2c70bf245d64f6ad039e8
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2019-08-15 18:02:21 +0200

    TW-61433: TeamCity ARM template: windows docker container has no runners
     - Remove unnecessary 1903 windows

--- build.ps1
+++ build.ps1
@@ -1,12 +1,9 @@
-$nanoserverTags = @("1803", "1809", "1903")
-$servercoreTags = @("1803", "1809")
+$tags = @("1803", "1809")
 
-foreach ($tag in $nanoserverTags) {
+foreach ($tag in $tags) {
  docker build `
   -t "teamcity-agent:latest-windowsservercore-$tag" `
   -f "windowsservercore/$tag/Dockerfile" .
-}
-foreach ($tag in $nanoserverTags) {
  docker build `
   -t "teamcity-agent:latest-nanoserver-$tag" `
   -f "nanoserver/$tag/Dockerfile" .
